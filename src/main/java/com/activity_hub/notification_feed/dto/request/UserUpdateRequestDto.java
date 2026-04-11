cc81006469c094e369cd6b5c59f43c99dff7ad8a
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-11 21:50:10 +0530

    feat: create UserUpdateRequestDto with validation constraints

---
+++ src/main/java/com/activity_hub/notification_feed/dto/request/UserUpdateRequestDto.java
@@ -0,0 +1,22 @@
+package com.activity_hub.notification_feed.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateRequestDto {
+    @NotBlank(message = "First name is required")
+    @Size(max = 100, message = "First name must not exceed 100 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 100, message = "Last name must not exceed 100 characters")
+    private String lastName;
+}
