4a8145ef9c52a4c153823b4e61fe5386c17e49b5
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-11 21:46:26 +0530

    feat: define UserService interface for user management and authentication workflows

---
+++ src/main/java/com/activity_hub/notification_feed/service/UserService.java
@@ -0,0 +1,20 @@
+package com.activity_hub.notification_feed.service;
+
+import com.activity_hub.notification_feed.dto.request.LoginRequestDto;
+import com.activity_hub.notification_feed.dto.request.PasswordRequestDto;
+import com.activity_hub.notification_feed.dto.request.UserRequestDto;
+import com.activity_hub.notification_feed.dto.request.UserUpdateRequestDto;
+import com.activity_hub.notification_feed.dto.response.LoginResponseDto;
+import com.activity_hub.notification_feed.dto.response.UserResponseDto;
+
+public interface UserService {
+   void createUser(UserRequestDto dto);
+   LoginResponseDto login(LoginRequestDto dto);
+   void resend(String email, String type);
+   void forgotPasswordSendVerificationCode(String email);
+   boolean verifyReset(String otp, String email);
+   boolean passwordReset(PasswordRequestDto dto);
+   boolean verifyEmail(String otp, String email);
+   void updateUserDetails(String email, UserUpdateRequestDto data);
+   UserResponseDto getUserDetails(String email);
+}
