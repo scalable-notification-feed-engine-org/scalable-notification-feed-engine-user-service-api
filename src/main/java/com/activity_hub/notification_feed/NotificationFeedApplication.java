d1aa07722603dd6c7c51b725a0cefc5833f900fb
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-11 20:16:08 +0530

    Initial commit👍

---
+++ src/main/java/com/activity_hub/notification_feed/NotificationFeedApplication.java
@@ -0,0 +1,13 @@
+package com.activity_hub.notification_feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/activity_hub/notification_feed/NotificationFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activity_hub.notification_feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
