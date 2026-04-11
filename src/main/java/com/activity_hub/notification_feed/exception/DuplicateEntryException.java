627f9f70f517048d157f66a6ccbbe7acca128668
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-11 22:32:25 +0530

    feat: create DuplicateEntryException for handling HTTP 409 Conflict errors

---
+++ src/main/java/com/activity_hub/notification_feed/exception/DuplicateEntryException.java
@@ -0,0 +1,11 @@
+package com.activity_hub.notification_feed.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class DuplicateEntryException extends RuntimeException {
+    public DuplicateEntryException(String message) {
+        super(message);
+    }
+}
