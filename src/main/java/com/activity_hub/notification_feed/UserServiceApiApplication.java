0dd407d94b488d3103cdf7b7a36b20e53cd494c1
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-11 20:26:29 +0530

    refactor: rename UserServiceApiApplication to NotificationFeedApplication

--- src/main/java/com/activity_hub/notification_feed/UserServiceApiApplication.java
+++ src/main/java/com/activity_hub/notification_feed/UserServiceApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationFeedApplication {
+public class UserServiceApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationFeedApplication.class, args);
+		SpringApplication.run(UserServiceApiApplication.class, args);
 	}
 
 }
