df7d1ed34cb0de8e98094e4c977d61d0301c7a32
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-12 12:03:56 +0530

    feat: implement FollowId embeddable component for composite primary key

---
+++ src/main/java/com/activity_hub/notification_feed/entity/FollowId.java
@@ -0,0 +1,17 @@
+package com.activity_hub.notification_feed.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class FollowId implements Serializable {
+    private UUID followerId;
+    private UUID followeeId;
+}
