16f793ef71815dddb29be49ebb508093cf017dec
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-12 15:31:46 +0530

    refactor: update USER_FOLLOW constant to SEND_FOLLOW for Kafka topic alignment

--- src/main/java/com/activity_hub/notification_feed/dto/event/EventTypes.java
+++ src/main/java/com/activity_hub/notification_feed/dto/event/EventTypes.java
@@ -1,5 +1,6 @@
 package com.activity_hub.notification_feed.dto.event;
 
-public final class UserEventTypes {
+public final class EventTypes {
     public static final String USER_OTP = "SEND_OTP";
+    public static final String USER_FOLLOW = "SEND_FOLLOW";
 }
