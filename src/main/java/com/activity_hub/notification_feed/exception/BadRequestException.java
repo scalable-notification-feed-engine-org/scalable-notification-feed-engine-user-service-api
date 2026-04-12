a2cd57ecae9d76583ede881c3be4ccddb2dcd57c
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-12 15:29:58 +0530

    feat: implement BadRequestException with Spring ResponseStatus annotation

---
+++ src/main/java/com/activity_hub/notification_feed/exception/BadRequestException.java
@@ -0,0 +1,11 @@
+package com.activity_hub.notification_feed.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
