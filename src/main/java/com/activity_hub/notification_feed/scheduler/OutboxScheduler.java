ddda31a170158a8f2d88a14ef71030886a64e16f
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-13 13:44:29 +0530

    feat: implement OutboxScheduler for asynchronous event publishing with fixed-rate polling

---
+++ src/main/java/com/activity_hub/notification_feed/scheduler/OutboxScheduler.java
@@ -0,0 +1,48 @@
+package com.activity_hub.notification_feed.scheduler;
+
+import com.activity_hub.notification_feed.dto.event.FollowEvent;
+import com.activity_hub.notification_feed.entity.OutboxEvent;
+import com.activity_hub.notification_feed.event.EventPublisher;
+import com.activity_hub.notification_feed.repository.OutboxRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OutboxScheduler {
+
+    private final OutboxRepository outboxRepository;
+    private final EventPublisher eventPublisher;
+    private final ObjectMapper objectMapper;
+
+    @Scheduled(fixedRate = 5000)
+    @Transactional
+    public void processOutboxEvents(){
+        List<OutboxEvent> pendingEvents = outboxRepository.findByStatusOrderByCreatedAtAsc("PENDING");
+
+        if(pendingEvents.isEmpty()){
+            return;
+        }
+
+        log.info("Processing {} outbox events...", pendingEvents.size());
+
+        pendingEvents.forEach(outboxEvent -> {
+            try {
+                FollowEvent followEvent = objectMapper.readValue(outboxEvent.getPayload(), FollowEvent.class);
+                eventPublisher.publishFollowUser(followEvent);
+                outboxEvent.setStatus("PROCESSED");
+                outboxRepository.save(outboxEvent);
+            } catch (Exception e) {
+                log.error("Failed to process outbox event ID: {}. Error: {}", outboxEvent.getId(), e.getMessage());
+            }
+        });
+
+    }
+}
