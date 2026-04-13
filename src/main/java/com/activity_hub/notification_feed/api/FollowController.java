ca728de5520644f9f2faedab451af4409189c1a8
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-13 13:42:09 +0530

    feat: implement FollowController for handling user follow actions via REST

---
+++ src/main/java/com/activity_hub/notification_feed/api/FollowController.java
@@ -0,0 +1,26 @@
+package com.activity_hub.notification_feed.api;
+
+import com.activity_hub.notification_feed.service.FollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/follows")
+@RequiredArgsConstructor
+public class FollowController {
+
+    private final FollowService followService;
+
+    @PostMapping("/{followeeId}")
+    public ResponseEntity<String> followUser(
+            @RequestHeader("X-User-Id") UUID followerId,
+            @PathVariable UUID followeeId) {
+        
+        followService.followUser(followerId, followeeId);
+        
+        return ResponseEntity.ok("Follow request processed successfully.");
+    }
+}
