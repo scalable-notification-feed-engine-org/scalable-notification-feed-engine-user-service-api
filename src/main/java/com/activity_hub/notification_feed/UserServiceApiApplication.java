fc846b49b96bdc95ff90318f901b7734b45225d7
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-13 13:45:59 +0530

    feat: enable scheduling for UserServiceApiApplication entry point

--- src/main/java/com/activity_hub/notification_feed/UserServiceApiApplication.java
+++ src/main/java/com/activity_hub/notification_feed/UserServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.activity_hub.notification_feed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class UserServiceApiApplication {
 
 	public static void main(String[] args) {
