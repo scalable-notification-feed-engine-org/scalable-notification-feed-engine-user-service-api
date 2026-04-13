7ddc8b527fc0fe145c9df52abcfe92e74baa5883
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-13 13:45:00 +0530

    feat: implement stateless security configuration with permitAll fallback

---
+++ src/main/java/com/activity_hub/notification_feed/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.activity_hub.notification_feed.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(autherize->{
+            autherize
+                    .anyRequest().permitAll();
+        });
+
+        http.sessionManagement(t->t.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // TASK 1
+        return http.build();
+
+    }
+}
