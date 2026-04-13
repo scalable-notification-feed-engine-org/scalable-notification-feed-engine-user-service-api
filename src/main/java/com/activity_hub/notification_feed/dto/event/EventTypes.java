fb6a2193723417912416ab9f6b926b61a229b437
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-13 16:13:42 +0530

    refactor: extend EventTypes for multi-topic follow/unfollow lifecycle

--- src/main/java/com/activity_hub/notification_feed/dto/event/EventTypes.java
+++ src/main/java/com/activity_hub/notification_feed/dto/event/EventTypes.java
@@ -3,4 +3,5 @@ package com.activity_hub.notification_feed.dto.event;
 public final class EventTypes {
     public static final String USER_OTP = "SEND_OTP";
     public static final String USER_FOLLOW = "SEND_FOLLOW";
+    public static final String USER_UNFOLLOW = "SEND_UNFOLLOW";
 }
