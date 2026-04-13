ba487393935b633739678d2a289bb3e4ef5a0256
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-13 13:41:22 +0530

    feat: implement EventConsumer with manual JSON parsing for flexible Kafka message handling

--- src/main/java/com/activity_hub/notification_feed/event/EventConsumer.java
+++ src/main/java/com/activity_hub/notification_feed/event/EventConsumer.java
@@ -3,6 +3,8 @@ package com.activity_hub.notification_feed.event;
 import com.activity_hub.notification_feed.dto.event.EventTypes;
 import com.activity_hub.notification_feed.dto.event.FollowEvent;
 import com.activity_hub.notification_feed.service.UserStatsService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,18 +16,23 @@ import org.springframework.stereotype.Component;
 public class EventConsumer {
 
     private final UserStatsService userStatsService;
+    private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = EventTypes.USER_FOLLOW,groupId = "${spring.kafka.consumer.group-id}")
-    public void consumeFollowEvent(FollowEvent event) {
-        log.info("Received follow event: {} follows {}", event.getFollowerId(), event.getFolloweeId());
+    public void consumeFollowEvent(String message) {
+        log.info("Received message: {}", message);
 
         try {
+            JsonNode jsonNode = objectMapper.readTree(message);
+            JsonNode dataNode = jsonNode.get("data");
+
+            FollowEvent event = objectMapper.treeToValue(dataNode, FollowEvent.class);
 
             userStatsService.updateStats(event.getFollowerId(), event.getFolloweeId());
 
         }catch (Exception e) {
 
-            log.error("Error updating stats for event: {}", event, e);
+            log.error("Error updating stats for event: {}", message, e);
 
         }
     }
