0637931c71ec73c944616014cb012ee311fc34a5
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-15 21:25:46 +0530

    feat(exception): implement domain-specific exception for keycloak integration

---
+++ src/main/java/com/activity_hub/notification_feed/exception/KeycloakException.java
@@ -0,0 +1,7 @@
+package com.activity_hub.notification_feed.exception;
+
+public class KeycloakException extends RuntimeException {
+    public KeycloakException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
