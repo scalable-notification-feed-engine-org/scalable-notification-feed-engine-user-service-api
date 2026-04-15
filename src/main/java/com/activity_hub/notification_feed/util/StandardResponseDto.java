267366ddfd89e01bbe89cd792702f0fb467e5cff
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-15 21:31:29 +0530

    feat(util): implement standardized API envelope for unified response structure

---
+++ src/main/java/com/activity_hub/notification_feed/util/StandardResponseDto.java
@@ -0,0 +1,14 @@
+package com.activity_hub.notification_feed.util;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StandardResponseDto {
+    private int code;
+    private String message;
+    private Object data;
+}
