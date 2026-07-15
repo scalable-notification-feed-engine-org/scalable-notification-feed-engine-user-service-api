317338b527b1c891f4b794e35fef1c5af37820c4
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-07-15 10:54:35 +0530

    refactor(profile): move presigned URL generation logic to service layer

--- src/main/java/com/activity_hub/notification_feed/api/ProfileImageController.java
+++ src/main/java/com/activity_hub/notification_feed/api/ProfileImageController.java
@@ -6,8 +6,6 @@ import com.activity_hub.notification_feed.service.impl.ProfileImageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/user-service/api/v1/profiles/media")
 public class ProfileImageController {
@@ -23,15 +21,12 @@ public class ProfileImageController {
             @RequestHeader("X-User-Id") String loggedInUserId,
             @RequestBody PresignedUrlRequestDto request) {
 
-        String objectKey = String.format("profiles/%s/%s_%s", 
-                loggedInUserId, request.getImageType(), UUID.randomUUID());
-
-        String uploadUrl = profileImageService.generatePresignedUploadUrl(
-                loggedInUserId, 
-                request.getImageType(), 
+        PresignedUrlResponseDto responseDto = profileImageService.generatePresignedUploadUrl(
+                loggedInUserId,
+                request.getImageType(),
                 request.getContentType()
         );
 
-        return ResponseEntity.ok(new PresignedUrlResponseDto(uploadUrl, objectKey));
+        return ResponseEntity.ok(responseDto);
     }
 }
